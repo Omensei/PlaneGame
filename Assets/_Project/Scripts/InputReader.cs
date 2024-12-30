aba16f1a34d04e238849f1c5db89308842224276
Omensei <omi.abdel@gmail.com>
2024-12-30 16:58:19 -0500

    added initial  player scripts

---
+++ Assets/_Project/Scripts/InputReader.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Shmup
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class InputReader : MonoBehaviour {
+
+        // NOTE: Make sure to set the player Input component to c# events
+
+        PlayerInput input;
+        InputAction moveAction;
+
+        public Vector2 Move => moveAction.ReadValue<Vector2>();
+
+        void Start ()
+        {
+            input = GetComponent<PlayerInput>();
+            moveAction = input.actions["Move"];
+        }
+
+
+    
+    
+    }
+
+
+
+}
---
+++ Assets/_Project/Scripts/ParallaxController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Shmup
+{
+    public class ParallaxController : MonoBehaviour { }
+
+
+
+}
